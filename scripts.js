e944805e66de782927195b350fa97110050a32d5
bryanosixtring <bryandavidr@gmail.com>
2023-10-06 15:48:47 -0500

    Ejericicos java

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
-let numero = 5;
-console.log(numero);
+let num =-5;
+
+if(num>0){
+    console.log(`${num} es mayor que 0`);
+    console.log(`${num} es mayor que 0`);
+
+}
+
+else{
+    console.log(`${num} es menor que 0`);
+
+}
